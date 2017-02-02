549641cb0b7f1b3559640cfb8da0cf2175358a4d
MaciejKawczynski <sqars89@gmail.com>
2017-02-02 20:35:33 +0100

    initial commit

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,40 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider){
+
+    var config = {
+      apiKey: "AIzaSyCvvgH59GHZZrHsM4d-Mef04_igfkIrtBM",
+      authDomain: "contacts-7c010.firebaseapp.com",
+      databaseURL: "https://contacts-7c010.firebaseio.com",
+      storageBucket: "contacts-7c010.appspot.com",
+      messagingSenderId: "502751562765"
+    };
+    
+    $firebaseRefProvider.registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    });
+    firebase.initializeApp(config);
+  })
+  .run(function($transitions, $state, AuthService){
+    $transitions.onStart({
+      to: function(state){
+        return !!(state.data && state.data.requiredAuth);
+      }
+    }, function(){
+      return AuthService
+        .requireAuthentication()
+        .catch(function(){
+          return $state.target('auth.login');
+        });
+    });
+    $transitions.onStart({
+      to: 'auth.*'
+    }, function(){
+      if(AuthService.isAuthenticated()){
+        return $state.target('app');
+      }
+    });
+  });
---
+++ src/app/components/auth/auth.service.js
@@ -0,0 +1,55 @@
+function AuthService($firebaseAuth){
+    var auth = $firebaseAuth();
+    var authData = null;
+
+    function storeAuthData(response){
+        authData = response;
+        return authData;
+    }
+
+    function onSignIn(user){
+        authData = user;
+        return auth.$requireSignIn();
+    }
+
+    function clearAuthData(){
+        authData = null;
+    }
+
+    this.register = function(user){
+        return auth
+            .$createUserWithEmailAndPassword(user.email, user.password)
+            .then(storeAuthData);
+    };
+
+    this.login = function(user){
+        return auth
+            .$signInWithEmailAndPassword(user.email, user.password)
+            .then(storeAuthData);
+    };
+
+    this.requireAuthentication = function(){
+        return auth.$waitForSignIn()
+            .then(onSignIn);
+    };
+
+    this.isAuthenticated = function(){
+        return !!authData; // null || {user}
+    };
+
+    this.getUser = function(){
+        if(authData){
+            return authData;
+        }
+    };
+
+    this.logout = function(){
+        return auth
+            .$signOut()
+            .then(clearAuthData);
+    }
+}
+
+angular
+    .module('components.auth')
+    .service('AuthService', AuthService);
